22c63099734f70cd1cfd2cb53d28fd32b6e7cb25
Sean Clark <sean.clark@pattern.com>
2026-07-01 09:11:57 -0400

    Add Convex + Vercel integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import { PUBLIC_CONVEX_URL } from '$env/static/public';
+  import { setupConvex } from 'convex-svelte';
 
-	let { children } = $props();
+  const { children } = $props();
+  setupConvex(PUBLIC_CONVEX_URL);
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
